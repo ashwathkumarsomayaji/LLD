5de11c069d8e4085e0e9fd349ce11d00bd793a19
ashwathkumarsomayaji <68903033+ashwathkumarsomayaji@users.noreply.github.com>
2025-04-28 19:29:06 +0530

    Create EventBooking.java

---
+++ Scheduler/EventBooking.java
@@ -0,0 +1,11 @@
+// Interview schedule + Slot booking
+
+// Event Booking System
+// Problem:
+// 	• Book time slots for events (e.g., meetings)
+// 	• Prevent double bookings
+// 	• Return available slots
+// Key Concepts:
+// 	• Calendar-like system
+// 	• Class: BookingSystem, Event
+// Conflict detection logic
