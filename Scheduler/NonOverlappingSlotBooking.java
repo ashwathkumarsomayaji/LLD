86f08e0f775f8f55e0c50706a32905e83e5b38c5
ashwathkumarsomayaji <68903033+ashwathkumarsomayaji@users.noreply.github.com>
2025-04-29 08:28:14 +0530

    Update NonOverlappingSlotBooking.java

--- Scheduler/NonOverlappingSlotBooking.java
+++ Scheduler/NonOverlappingSlotBooking.java
@@ -1,7 +1,52 @@
 package com.tdd.booking;
 import java.util.List;
 import java.util.Objects;
-public class BookingManagerImpl implements IBookingManager {
+
+ class BookingValidation{
+    public boolean validate(Slot slot) throws InvalidBookingTimeException {
+        boolean validSlot = true;
+        String startTime = slot.getStartDate();
+        String endTime = slot.getEndDate();
+        String[] startTimeArray = startTime.split(":");
+        if(startTimeArray.length != 2) {
+            return false;
+        } else {
+            int hoursInStartTime = Integer.parseInt(startTimeArray[0]);
+            if(hoursInStartTime < 1 || hoursInStartTime > 12) {
+                validSlot = false;
+            }
+            String[] minutesAndTimeFormatInStartTime = startTimeArray[1].split("\\s");
+
+	This line splits the second part of the time string (which contains minutes and AM/PM) by whitespace (\\s+), to separate:
+
+
+            if(!minutesAndTimeFormatInStartTime[0].equals("00"))
+                validSlot = false;
+            else if(!(minutesAndTimeFormatInStartTime[1].equals("AM") || minutesAndTimeFormatInStartTime[1].equals("PM")))
+                validSlot = false;
+        }
+        String[] endTimeArray = endTime.split(":");
+        if(endTimeArray.length != 2) {
+            validSlot = false;
+        } else {
+            int hoursInEndTime = Integer.parseInt(endTimeArray[0]);
+            if(hoursInEndTime < 1 || hoursInEndTime > 12) {
+                validSlot = false;
+            }
+            String[] minutesAndTimeFormatInEndTime = endTimeArray[1].split("\\s");
+            if(!minutesAndTimeFormatInEndTime[0].equals("00"))
+                validSlot = false;
+            else if(!(minutesAndTimeFormatInEndTime[1].equals("AM") || minutesAndTimeFormatInEndTime[1].equals("PM")))
+                validSlot = false;
+        }
+        if(!validSlot)
+            throw new InvalidBookingTimeException("There is invalid time format in slot" + slot.toString() + "Please check the guidelines");
+        return true;
+    }
+}
+
+
+public class NonOverlappingSlotBooking {
     List<Slot> slots;
     IBookingValidation bookingValidation;
     String bookingType;
